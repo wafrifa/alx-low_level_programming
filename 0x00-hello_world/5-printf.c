e5d4d97b9ac7131143b2502952081d2bbea3745f
William Amoah m Amoah Afrifa <kwasiamoa93@iamoa93@icloud.com>
2022-10-15 15:12:18 -0700

    Sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stidio.h>
+/**
+ * main - main is the entry for code
+ * Returns: return 0 is used to exit the code
+ */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);}
+{
+	printf("with proper grammar, but the outcome is a piece of art,);
+	return (0)
+} 
