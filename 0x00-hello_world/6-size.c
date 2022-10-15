c04472c61e3bff924ba6b9a1be1c9c7737257765
William Amoah m Amoah Afrifa <kwasiamoa93@iamoa93@icloud.com>
2022-10-15 16:06:10 -0700

    Seventh commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main is the entry for code
+ * Return: return 0 is used to exit the code
+ */
+int main(void)
+{int a; long int b; long long int c; char d; float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
