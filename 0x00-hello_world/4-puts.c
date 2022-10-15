a7160d9de8b346d8059b521fd911009973aca515
William Amoah m Amoah Afrifa <kwasiamoa93@iamoa93@icloud.com>
2022-10-15 15:04:00 -0700

    Fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - main is the entry for code
+ * Return: return 0 is used to exit the code
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
