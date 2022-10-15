90b6b932c03dd1f522cabda107a5c144897870e8
William Amoah m Amoah Afrifa <kwasiamoa93@iamoa93@icloud.com>
2022-10-15 14:14:48 -0700

    Fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+ * * main - Entry
+ * *before* Return: Always 0
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
